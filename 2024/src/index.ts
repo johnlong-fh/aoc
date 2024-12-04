d877069c7b6ddf152bc6cefee97f975cd8322537
John Long <john.long@foundationhealth.com>
2024-12-03 22:33:23 -0600

    refactor entry point

--- 2024/src/index.ts
+++ 2024/src/index.ts
@@ -1,39 +1,89 @@
-import { partOne as dayOnePartOne, partTwo as dayOnePartTwo } from './day-1';
-import { partOne as dayTwoPartOne, partTwo as dayTwoPartTwo } from './day-2';
-import {
-  partOne as dayThreePartOne,
-  partTwo as dayThreePartTwo,
-} from './day-3';
+import { promises as fs } from 'fs';
 
-const args = process.argv.slice(2);
+type PartFunction = () => Promise<any>;
 
-if (args.length === 0) {
-  console.error('Please provide a function name as an argument.');
-  process.exit(1);
+interface FunctionMap {
+  [key: string]: PartFunction;
 }
 
-const functionName = args[0];
+const formatMemoryUsage = (mem: number) =>
+  `${Math.round((mem / 1024 / 1024) * 100) / 100} MB`;
 
-switch (functionName) {
-  case 'day-one-pt-1':
-    dayOnePartOne().then((result) => console.log(result));
-    break;
-  case 'day-one-pt-2':
-    dayOnePartTwo().then((result) => console.log(result));
-    break;
-  case 'day-two-pt-1':
-    dayTwoPartOne().then((result) => console.log(result));
-    break;
-  case 'day-two-pt-2':
-    dayTwoPartTwo().then((result) => console.log(result));
-    break;
-  case 'day-three-pt-1':
-    dayThreePartOne().then((result) => console.log(result));
-    break;
-  case 'day-three-pt-2':
-    dayThreePartTwo().then((result) => console.log(result));
-    break;
-  default:
+async function loadFunctions(): Promise<FunctionMap> {
+  const functionMap: FunctionMap = {};
+  const dayRegex = /^day-(\d+)$/;
+  const files = await fs.readdir(__dirname);
+
+  for (const file of files) {
+    const match = file.match(dayRegex);
+    if (match) {
+      const dayNumber = match[1];
+
+      try {
+        const partOneModule = await import(`./${file}/part-1`);
+        functionMap[`day-${dayNumber}-pt-1`] = partOneModule.partOne;
+      } catch {
+        console.warn(`part-1 not found for day ${dayNumber}`);
+      }
+
+      try {
+        const partTwoModule = await import(`./${file}/part-2`);
+        functionMap[`day-${dayNumber}-pt-2`] = partTwoModule.partTwo;
+      } catch {
+        console.warn(`part-2 not found for day ${dayNumber}`);
+      }
+    }
+  }
+  return functionMap;
+}
+
+(async () => {
+  const functionMap = await loadFunctions();
+
+  const args = process.argv.slice(2);
+  if (args.length === 0) {
+    console.error('Please provide a function name as an argument.');
+    process.exit(1);
+  }
+
+  const functionName = args[0];
+  const func = functionMap[functionName];
+
+  if (func) {
+    try {
+      const startTime = process.hrtime.bigint();
+
+      const result = await func();
+
+      const endTime = process.hrtime.bigint();
+
+      const duration = Number(endTime - startTime) / 1e6;
+
+      const memoryData = process.memoryUsage();
+
+      const memoryUsage = {
+        rss: formatMemoryUsage(memoryData.rss),
+        heapTotal: formatMemoryUsage(memoryData.heapTotal),
+        heapUsed: formatMemoryUsage(memoryData.heapUsed),
+        external: formatMemoryUsage(memoryData.external),
+      };
+
+      console.log(
+        JSON.stringify(
+          {
+            Result: result,
+            ExecutionTime: `${duration.toFixed(2)} ms`,
+            ...memoryUsage,
+          },
+          undefined,
+          2,
+        ),
+      );
+    } catch (error) {
+      console.error(`Error executing function ${functionName}:`, error);
+    }
+  } else {
     console.error(`Unknown function: ${functionName}`);
     process.exit(1);
-}
+  }
+})();
